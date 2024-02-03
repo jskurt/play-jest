d0543a402114a646137940ada7e32d35517ac01a
Jisung Lee <jskurt9352@gmail.com>
2024-02-03 23:50:43 +0900

    modified fn.js and fn.test.js

--- fn.js
+++ fn.js
@@ -1,6 +1,24 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
-  makeUser: (name, age) => ({ name, age, gender: undefined })
+  makeUser: (name, age) => ({ name, age, gender: undefined }),
+  throwError: () => {
+    throw new Error('xx')
+  },
+  getName: (callback) => {
+    const name = 'Jisung'
+    setTimeout(() => {
+      callback(name)
+      throw new Error('This is server error')
+    }, 3000)
+  },
+  getAge: () => {
+    const age = 30
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(age)
+      }, 3000)
+    })
+  }
 }
 
 module.exports = fn
--- fn.test.js
+++ fn.test.js
@@ -48,5 +48,87 @@ test('defined is defined', () => {
 })
 
 // toBeTruthy
+test('1 and string is truthy', () => {
+  expect(1).toBeTruthy()
+  expect(fn.add('hello', 'world')).toBeTruthy()
+})
 
 // toBeFalsy
+test('0 is truthy', () => {
+  expect(0).toBeFalsy()
+})
+
+// toBeGreaterThan
+// toBeGreaterThanOrEqual
+// toBeLessThan
+// toBeLessThanOrEqual
+
+test('ID less than 10', () => {
+  const id = 'JISUNG_LEE'
+  expect(id.length).toBeLessThanOrEqual(10)
+})
+
+test('PW length is 4', () => {
+  const pw = '1234'
+  expect(pw.length).toBe(4)
+})
+
+test('PW length is 4', () => {
+  const pw = '1234'
+  expect(pw.length).toEqual(4)
+})
+
+test('0.1 + 0.2 = 0.3', () => {
+  expect(fn.add(0.1, 0.2)).toBeCloseTo(0.3)
+})
+
+test('Does it has a in Hello world?', () => {
+  expect('Hello, world').toMatch(/H/)
+})
+
+test('Does it has Jisung in user list?', () => {
+  const user = 'Jisung'
+  const userList = ['Jisung', 'Jenny', 'Kurt']
+  expect(userList).toContain(user)
+})
+
+test('Does it occur error?', () => {
+  expect(() => fn.throwError()).toThrow('xx')
+})
+
+test('received name is Jisung after 3 second', (done) => {
+  function callback(name) {
+    expect(name).toBe('Jisung')
+    done()
+  }
+  fn.getName(callback)
+})
+
+// if you want check API errors
+test('received name is Jisung after 3 second', (done) => {
+  function callback(name) {
+    try {
+      expect(name).toBe('Jisung')
+      done()
+    } catch (error) {
+      done()
+    }
+  }
+  fn.getName(callback)
+})
+
+test('received age is 30 after 3 second', () => {
+  return fn.getAge().then((age) => {
+    expect(age).toBe(30)
+  })
+})
+
+// resolves rejects
+test('received age is 30 after 3 second', () => {
+  return expect(fn.getAge()).resolves.toBe(30)
+})
+
+test('received age is 30 after 3 second', async () => {
+  const age = await fn.getAge()
+  expect(age).toBe(30)
+})
