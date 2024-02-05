c99f448143723ebbc7020377296282b44ab6c0fb
Jisung Lee <jskurt9352@gmail.com>
2024-02-05 10:33:23 +0700

    modify beforeEach and afterEach

--- fn.test.js
+++ fn.test.js
@@ -134,3 +134,27 @@ test('received age is 30 after 3 second', async () => {
 })
 
 // beforeEach, afterEach
+let num = 0
+
+beforeEach(() => {
+  num = 0
+})
+test('0 + 1 = 1', () => {
+  num = fn.add(num, 1)
+  expect(num).toBe(1)
+})
+
+test('0 + 2 = 2', () => {
+  num = fn.add(num, 2)
+  expect(num).toBe(2)
+})
+
+test('0 + 3 = 3', () => {
+  num = fn.add(num, 3)
+  expect(num).toBe(3)
+})
+
+test('0 + 4 = 4', () => {
+  num = fn.add(num, 4)
+  expect(num).toBe(4)
+})
